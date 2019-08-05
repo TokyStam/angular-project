54f2e690988754bcf75ca705bccc4f67b92a7fb1
TokyStam <johnnyftr05@gmail.com>
2019-08-05 12:35:25 +0300

    utiliser @Input(), variable dynamyque

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  appareilStatu ="Eteind";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getStatus(){
+    return this.appareilStatu;
+  }
+
+}
